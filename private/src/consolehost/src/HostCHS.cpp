e38e1790a7ca87fea4b6c79f51629d65b659a832
Amlal El Mahrouss <amlal@nekernel.org>
2026-09-16 06:23:29 +0200

    CHORE: src: refactor console window management in HostCHS.cpp for clarity and consistency

--- private/src/consolehost/src/HostCHS.cpp
+++ private/src/consolehost/src/HostCHS.cpp
@@ -7,27 +7,33 @@
 #include <Ne/System/LWAS.h>
 
 static struct LHANDLE* kConsoleWnd{};
-static VoidPtr kConsoleWindowPtr{};
-static SizeT kConsoleWindowSz{};
+static VoidPtr         kConsoleWindowPtr{};
+static SizeT           kConsoleWindowSz{};
 
-IMPORT_C SInt32 CHSOpenConsole(Void)
-{
-    if (!kConsoleWindowSz || !kConsoleWindowPtr) return kErrorInvalidData;
+IMPORT_C SInt32 CHSCloseConsole(Void) {
+  if (!kConsoleWindowSz || !kConsoleWindowPtr) return kErrorInvalidData;
 
-    if (auto ret = UsrCreateWindow(kConsoleWindowPtr, &kConsoleWindowSz); ret)
-    {
-        kConsoleWnd = ret;
-        return kErrorSuccess;
-    }
+  UsrDestroyWindow(kConsoleWnd);
+  kConsoleWnd = nullptr;
 
-    return kErrorInvalidData;
+  return kErrorSuccess;
 }
 
-IMPORT_C SInt32 CHSHandleListen(Void)
-{
-    if (!kConsoleWnd) return kErrorInvalidData;
-    
-    while (kConsoleWnd);
+IMPORT_C SInt32 CHSOpenConsole(Void) {
+  if (!kConsoleWindowSz || !kConsoleWindowPtr) return kErrorInvalidData;
 
+  if (auto ret = UsrCreateWindow(kConsoleWindowPtr, &kConsoleWindowSz); ret) {
+    kConsoleWnd = ret;
     return kErrorSuccess;
+  }
+
+  return kErrorInvalidData;
+}
+
+IMPORT_C SInt32 CHSHandleListen(Void) {
+  if (!kConsoleWnd) return kErrorInvalidData;
+
+  while (kConsoleWnd);
+
+  return kErrorSuccess;
 }
